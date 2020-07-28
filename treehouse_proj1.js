f2327a5867c658a14df03c078e14bc0e4baa8ad3
sethlewis93 <64318680+sethlewis93@users.noreply.github.com>
2020-07-28 11:31:00 -0400

    Removed for in loops

---
+++ treehouse_proj1.js
@@ -0,0 +1,19 @@
+/*
+Build a collection of quotations — these are the quotes, author and other information that gets printed to the screen. 
+You'll do this by creating a JavaScript array containing JavaScript objects — one object for each quotation.
+Create a function that pulls one of those quotes out at random.
+Create a function that prints that quote to the screen.
+
+*/
+
+const quotations = [{quote: 'Too many pieces of music finish too long after the end', 
+                     source: 'Stravinsky, Igor', 
+                     citation: 'Byrne, Robert: "...Best Things Ever Said"'}, 
+                    {quote: 'Finishing a book is just like you took a child out in the back yard and shot it', 
+                     source: 'Capote, Truman', 
+                     citation: 'Byrne, Robert: "...Best Things Ever Said"'}, 
+                    {quote: 'I love America. You always hurt the one you love', 
+                     source: 'Frye, David', 
+                     citation: 'Byrne, Robert: "...Best Things Ever Said"'}
+];
+
